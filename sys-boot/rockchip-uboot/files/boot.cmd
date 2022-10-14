3ade3b365fa7af37330f2891bd83ab0c4628f3f7
Su Yue <glass@fydeos.io>
2022-10-14 12:54:18 +0800

    rockchip-uboot: add support for dtbo
    Signed-off-by: Su Yue <glass@fydeos.io>

--- sys-boot/rockchip-uboot/files/boot.cmd
+++ sys-boot/rockchip-uboot/files/boot.cmd
@@ -11,6 +11,25 @@ fi
 
 part uuid ${devtype} ${devnum}:${rootpart} root_uuid
 setenv bootargs rootwait ro cros_debug cros_secure cros_legacy console=ttyS2,1500000n8 root=PARTUUID=${root_uuid}
-load ${devtype} ${devnum}:${rootpart} ${kernel_addr_r} ${linux_image}
+
 load ${devtype} ${devnum}:${rootpart} ${fdt_addr_r} ${fdtfile}
+
+setenv distro_bootpart 1
+if load ${devtype} ${devnum}:${distro_bootpart} ${kernel_addr_r} /unencrypted/overlays.txt; then
+   env import -t ${kernel_addr_r} $filesize}
+   fdt addr ${fdt_addr_r}
+   fdt resize
+
+   for ov in ${overlays}; do
+	   if load ${devtype} ${devnum}:${rootpart} ${kernel_addr_r} /boot/overlays/${ov}.dtbo; then
+		  fdt apply ${kernel_addr_r}
+		  echo loaded overlay ${ov}...
+	   else
+		  echo ${ov}.dtbo not found in /boot/overlays
+	   fi
+   done
+fi
+
+load ${devtype} ${devnum}:${rootpart} ${kernel_addr_r} ${linux_image}
+
 booti ${kernel_addr_r} - ${fdt_addr_r}
